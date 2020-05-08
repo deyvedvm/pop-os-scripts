6ebdda250143e887a980b5a05b47f2bdfaf8c477
Deyve Vieira <deyvedvm@hotmail.com>
2020-05-07 21:50:10 -0300

    Install Golang Language

--- pop-os-script.sh
+++ pop-os-script.sh
@@ -199,10 +199,17 @@ sudo apt-get install xclip
 
 # Install Golang Language
 
-# TODO
+# Download from  https://golang.org/dl/
+
+sudo tar -C /usr/local -xzf go1.14.2.linux-amd64.tar.gz
+
+# export PATH=$PATH:/usr/local/go/bin
+
+source .bashrc
+
+
 
 # Games
-
 
 # Install Steam
 
