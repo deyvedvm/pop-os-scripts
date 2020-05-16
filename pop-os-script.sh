6c86542d99d8cb4b375c535f967b70c23654af44
Deyve Vieira <deyvedvm@hotmail.com>
2020-05-16 17:50:34 -0300

    Install SpeedTest

--- pop-os-script.sh
+++ pop-os-script.sh
@@ -42,6 +42,10 @@ sudo apt install ubuntu-restricted-extras
 
 sudo apt install ffmpeg
 
+# Install SpeedTest - Check internet speed
+
+sudo apt-get install speedtest-cli
+
 # Install Open Broadcaster Software - OBS
 
 sudo apt install obs-studio
