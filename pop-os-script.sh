a1de86ff0f75140ce592db8c8b7e0db3378637fe
Deyve Vieira <deyvedvm@hotmail.com>
2020-05-23 19:50:45 -0300

    Install Portainer IO

--- pop-os-script.sh
+++ pop-os-script.sh
@@ -168,6 +168,13 @@ sudo apt install -y docker.io
 
 sudo apt install docker-compose
 
+# Install Portainer IO
+
+docker volume create portainer_data
+
+docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
+
+
 # Install Visual Studio Code
 
 sudo apt-get install code
